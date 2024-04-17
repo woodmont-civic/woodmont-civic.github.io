61dfabf1b4bfc7e3deb79b848e2c3676bdb4abdc
Gabriel Duke <gabeduke@gmail.com>
2024-04-17 09:45:20 -0400

    reorg (#35)
    reorganize the newsletters -> Posts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,5 +4,15 @@ export default defineNuxtConfig({
       failOnError: false,
     }
   },
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: 'en'
+      },
+      meta: [
+        {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+      ],
+    },
+  },
   extends: 'content-wind',
 })
