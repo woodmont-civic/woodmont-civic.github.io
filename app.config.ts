26bfdb5185dfa18c484c6738d4fca15444774b36
Gabriel Duke <gabeduke@gmail.com>
2025-08-28 23:52:51 -0400

    add covenant, update landing img (#37)
    * add covenant, update landing img
    
    * bump deps
    
    * release flow

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,6 @@
 export default defineAppConfig({
   // Configure Content Wind here
-  cover: '/cover.jpg',
+  cover: '/cover.png',
   socials: {
     github: 'woodmont-civic/woodmont-civic.github.io',
   },
